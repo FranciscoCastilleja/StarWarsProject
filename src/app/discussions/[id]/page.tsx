abaa3bf29ee81f2af5448c008cbe57fa4a745e65
FranciscoCastilleja <fran_c555@hotmail.com>
2024-04-15 18:22:02 -0500

    Try fix show the new comment 4

--- src/app/discussions/[id]/page.tsx
+++ src/app/discussions/[id]/page.tsx
@@ -13,7 +13,7 @@ export default function IdDiscussion() {
 
     const supabase = createClient()
 
-    const [messageShow, setMessageShow] = useState<string | null>(null)
+    const [messageShow, setMessageShow] = useState<boolean>(false)
     const [user, setUser] = useState<User | null>(null)
 
     useEffect(() => {
@@ -27,11 +27,12 @@ export default function IdDiscussion() {
         GetUser()
     }, [])
 
+    const searchParams = useSearchParams()
+    const search = searchParams.get('message')
+
     useEffect(() => {
-        const searchParams = useSearchParams()
-        const search = searchParams.get('message')
         if (search !== undefined || search !== null) {
-            setMessageShow(search)
+            setMessageShow(true)
         }
     }, [])
 
@@ -39,7 +40,7 @@ export default function IdDiscussion() {
         <main className="min-h-screen h-auto">
             <div className="grid size-full place-items-center">
                 {
-                    messageShow !== '' && (
+                    messageShow === true && (
                         <div className="flex absolute top-36 items-center w-full max-w-xs p-4 mb-4 rounded-lg shadow text-gray-400 bg-[#333]" role="alert">
                             <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-lg bg-red-800 text-red-200">
                                 <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -47,8 +48,8 @@ export default function IdDiscussion() {
                                 </svg>
                                 <span className="sr-only">Error icon</span>
                             </div>
-                            <div className="ms-3 text-sm font-normal">{messageShow}.</div>
-                            <button onClick={() => setMessageShow(null)} className="ms-auto -mx-1.5 -my-1.5 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 inline-flex items-center justify-center h-8 w-8 text-gray-500 hover:text-white bg-[#222] hover:bg-[#444]" aria-label="Close">
+                            <div className="ms-3 text-sm font-normal">{search}.</div>
+                            <button onClick={() => setMessageShow(false)} className="ms-auto -mx-1.5 -my-1.5 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 inline-flex items-center justify-center h-8 w-8 text-gray-500 hover:text-white bg-[#222] hover:bg-[#444]" aria-label="Close">
                                 <span className="sr-only">Close</span>
                                 <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
