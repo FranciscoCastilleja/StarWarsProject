aeac37921400e6e4850056ff5fe5fc877ebdd991
FranciscoCastilleja <fran_c555@hotmail.com>
2024-04-16 15:25:14 -0500

    Change the card

--- src/app/componentes/Card.tsx
+++ src/app/componentes/Card.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Card({name, year, user}:{name:string, year:string, user:boolean}) {
     return(
-        <div className="grid my-5 w-60 h-auto bg-[#333] rounded-3xl place-items-center hover:bg-[#444]/90">
+        <div className="grid my-5 px-1 w-72 h-auto bg-[#333] rounded-3xl place-items-center border border-white/10 hover:bg-[#444]/90">
             <h2 className="mt-3 mb-2 text-white text-center text-lg font-bold">{name}</h2>
             <p className="mb-2 text-white text-base">{year}</p>
             {
