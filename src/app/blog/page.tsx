9d31c7339f8988fb3e012578da8998cf1bfc6f30
FranciscoCastilleja <fran_c555@hotmail.com>
2024-04-18 18:17:55 -0500

    Adding more content in blog 2

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,6 +2,11 @@ export default function Blog() {
     return (
         <main className="grid min-h-screen h-auto place-items-center">
             <article className="my-3 w-[90%] h-fit border-y border-white sm:w-[50%]">
+                <h2 className="my-2 text-white text-xl font-bold">Problem Solved</h2>
+                <p className="my-2 text-white text-base">The problem about the comments was solved, now you can send comments and watch it in realtime.</p>
+                <p className="my-2 text-white text-center text-base">Attn: Francisco Castilleja (Creator)</p>
+            </article>
+            <article className="my-3 w-[90%] h-fit border-y border-white sm:w-[50%]">
                 <h2 className="my-2 text-white text-xl font-bold">Warning</h2>
                 <p className="my-2 text-white text-base">There is a problem, when you send a comment in a discussion, you need refresh the page to see the new comments.</p>
                 <p className="my-2 text-white text-base">I hope solve this problem in the future.</p>
