ec0cb2532305f66e043f20a682f67afdee2be783
FranciscoCastilleja <fran_c555@hotmail.com>
2024-06-01 20:27:53 -0500

    Add update status discussion

--- src/app/componentes/sections/CloseDiscussionButton.tsx
+++ src/app/componentes/sections/CloseDiscussionButton.tsx
@@ -36,10 +36,17 @@ export default function CloseDiscussionButton({id}:{id:string}) {
         getData()
     })
 
+    async function ChangeStatus() {
+        const { error } = await supabase
+        .from('Discussions')
+        .update({ status: false })
+        .eq('id', id)
+    }
+
     return (
         <section className="flex mt-3 w-full h-auto justify-center">
             {
-                user?.user_metadata.full_name === data.created_by && (<button className="mb-3 p-2 text-white text-base bg-black/90 border border-white rounded-xl hover:bg-red-700 hover:border-red-700">Close Discussion</button>)
+                user?.user_metadata.full_name === data.created_by && data.status != false && (<button onClick={ChangeStatus} className="mb-3 p-2 text-white text-base bg-black/90 border border-white rounded-xl hover:bg-red-700 hover:border-red-700">Close Discussion</button>)
             }
         </section>
     );
